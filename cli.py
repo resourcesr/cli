b98f705978e76f463599344f7bba7bfb33d8e860
lablnet <mumerfarooqlablnet01@gmail.com>
2020-09-17 16:11:21 +0500

    fix

--- cli.py
+++ cli.py
@@ -1,14 +1,18 @@
 from src._config import get_config
-import urllib.request, urllib.parse, urllib.error
+from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
 import ssl
 
-ctx = ssl.create_default_context()
+"""ctx = ssl.create_default_context()
 ctx.check_hostname = False
-ctx.verify_mode = ssl.CERT_NONE
+ctx.verify_mode = ssl.CERT_NONE"""
 
+heads = {'User-Agent': 'python'}
 url = "https://www.riphah.edu.pk/faculty/ict-computing" #get_config("url")
-html = urllib.request.urlopen(url, context = ctx).read()
+req = Request(url, headers={'User-Agent': 'PYTHON/3.8'})
+response = urlopen(req)
+html = response.read().decode()
+
 soup = BeautifulSoup(html, 'html.parser')
 
 print(soup)
